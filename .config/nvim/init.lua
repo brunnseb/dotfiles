ae4d8976cf66840806239a3142874b7f2ebe0d6d
Sebastian Brunner <sebastian.brunner@ti8m.ch>
2023-01-24 09:17:25 +0100

    Initialize lazyvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,18 +1,2 @@
-local opts = { noremap = true, silent = true }
-
-local keymap = vim.api.nvim_set_keymap
-
---Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
-
-vim.g.mapleader = " "
-
-vim.g.maplocalleader = " "
-
-require("options")
-require("config._lazy")
-require("keymaps")
-require("autocommands")
-require("lsp.setup")
-require("lsp.diagnostics")
-require("colorscheme")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
