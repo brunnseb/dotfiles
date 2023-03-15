ec1381c3c4a8138f7a5f132f3281691cf85bcefb
Sebastian Brunner <seba.brunner@proton.me>
2023-03-15 06:34:57 +0100

    Use astrovim

--- .config/nvim/lua/plugins/disabled.lua
+++
@@ -1,8 +0,0 @@
-return {
-  { "akinsho/bufferline.nvim", enabled = false },
-  { "SmiteshP/nvim-navic", enabled = false },
-  { "echasnovski/mini.surround", enabled = false },
-  { "rafamadriz/friendly-snippets", enabled = false },
-  { "ggandor/flit.nvim", enabled = false },
-  { "ggandor/leap.nvim", enabled = false },
-}
