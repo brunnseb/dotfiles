e259f16162c2549ec5267f345dc0e8942c1d5418
Sebastian Brunner <seba.brunner@proton.me>
2023-04-23 21:18:01 +0200

    Use nightly channel

--- .config/astronvim/lua/user/polish.lua
+++ .config/astronvim/lua/user/polish.lua
@@ -1,6 +1,19 @@
+-- Keymaps
 local keymap = vim.keymap.set
 
 keymap({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
 keymap({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-w" })
 keymap({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-w" })
 keymap({ "n", "o", "x" }, "ge", "<cmd>lua require('spider').motion('ge')<CR>", { desc = "Spider-w" })
+
+-- Autocmds
+vim.api.nvim_create_autocmd("WinLeave", {
+  desc = "Prevent opening files from telescope in insert mode",
+  -- group = vim.api.nvim_create_augroup("autocomp", { clear = true }),
+  pattern = "*",
+  callback = function()
+    if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+    end
+  end,
+})
