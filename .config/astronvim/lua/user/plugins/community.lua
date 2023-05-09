3e68b2bf7bb1099418614e52879d9f405d06b94b
Sebastian Brunner <sebastian.brunner@ti8m.ch>
2023-05-09 08:08:09 +0200

    Update config

--- .config/astronvim/lua/user/plugins/community.lua
+++ .config/astronvim/lua/user/plugins/community.lua
@@ -234,11 +234,11 @@ return {
   -- Language packs
   -- { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.tailwindcss" },
-  { import = "astrocommunity.pack.bash" },
+  -- { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.python" },
+  -- { import = "astrocommunity.pack.markdown" },
+  -- { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.yaml" },
   -- Utility
   { import = "astrocommunity.utility.neodim" },
