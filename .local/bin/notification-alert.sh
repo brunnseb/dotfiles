dc202b4b970aef44005492ea14e8c1b8b390d838
Sebastian <seba.brunner@proton.me>
2023-08-15 21:54:25 +0200

    Refactor hyprland config

--- .local/bin/notification-alert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dbus-monitor --profile "type=method_call,interface=org.freedesktop.Notifications,member=Notify" |
-  while ((i++));read -r line; do
-    if [ $i -gt 4 ]
-    then
-      paplay /usr/share/sounds/freedesktop/stereo/message.oga
-    fi
-  done
