19237df4882991258930dfada0fd2e2094f14ccd
Sebastian <sebastian.brunner@ti8m.ch>
2023-12-06 07:33:58 +0100

    Update nvim config

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -36,28 +36,54 @@ return {
       require('catppuccin').setup {
         flavour = 'macchiato', -- mocha, macchiato, frappe, latte
         integrations = {
-          semantic_tokens = true,
           alpha = true,
-          mini = true,
-          markdown = true,
-          lsp_trouble = false,
-          rainbow_delimiters = true,
-          gitsigns = true,
-          mason = true,
-          neotree = true,
-          neotest = true,
-          noice = true,
           cmp = true,
-          notify = true,
-          treesitter_context = true,
-          treesitter = true,
-          symbols_outline = true,
-          which_key = true,
-          -- lsp_saga = true,
+          dashboard = true,
+          flash = true,
+          gitsigns = true,
           indent_blankline = {
             enabled = true,
             colored_indent_levels = false,
           },
+          lsp_trouble = false,
+          markdown = true,
+          mason = true,
+          mini = {
+            enabled = true,
+            indentscope_color = 'teal'
+          },
+          dap = {
+            enabled = true,
+            enable_ui = true
+          },
+          native_lsp = {
+            enabled = true,
+            virtual_text = {
+              errors = { "italic" },
+              hints = { "italic" },
+              warnings = { "italic" },
+              information = { "italic" },
+            },
+            underlines = {
+              errors = { "underline" },
+              hints = { "underline" },
+              warnings = { "underline" },
+              information = { "underline" },
+            },
+            inlay_hints = {
+              background = true,
+            },
+          },
+          neotest = true,
+          neotree = true,
+          noice = true,
+          notify = true,
+          rainbow_delimiters = true,
+          semantic_tokens = true,
+          symbols_outline = true,
+          treesitter = true,
+          treesitter_context = true,
+          which_key = true,
         },
         highlight_overrides = {
           macchiato = function(macchiato)
