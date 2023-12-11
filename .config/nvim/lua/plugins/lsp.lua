e086327a71219227d92e0cace9408b5d7d242e1b
Sebastian Brunner <seba.brunner@proton.me>
2023-12-11 12:43:33 +0100

    Update nvim conf

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,17 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- change a keymap
+      -- keys[#keys + 1] = { "K", "<cmd>echo 'hello'<cr>" }
+      -- disable a keymap
+      keys[#keys + 1] = { "gr", false }
+      keys[#keys + 1] = { "gy", false }
+      keys[#keys + 1] = { "gd", false }
+      -- add a keymap
+      -- keys[#keys + 1] = { "H", "<cmd>echo 'hello'<cr>" }
+    end,
     opts = {
       servers = {},
       setup = {
