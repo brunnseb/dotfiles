2bd300a09533899a8d6be64d90f970415b852752
Sebastian <sebastian.brunner@ti8m.ch>
2024-04-24 20:57:46 +0200

    Update css lsp settings

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -87,6 +87,19 @@ return {
       })
 
       local servers = {
+        cssls = {
+          settings = {
+            css = { validate = true, lint = {
+              unknownAtRules = 'ignore',
+            } },
+            scss = { validate = true, lint = {
+              unknownAtRules = 'ignore',
+            } },
+            less = { validate = true, lint = {
+              unknownAtRules = 'ignore',
+            } },
+          },
+        },
         eslint = {
           settings = {
             workingDirectories = { mode = 'auto' },
