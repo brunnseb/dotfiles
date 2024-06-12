5a498f4609829e0cb7c6c3e8ba702e34f91ff41a
Sebastian Brunner <seba.brunner@proton.me>
2024-06-12 13:59:16 +0200

    Add dap

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -32,6 +32,7 @@ return {
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-nvim-lsp',
       'onsails/lspkind-nvim',
+      'rcarriga/cmp-dap',
       -- 'windwp/nvim-autopairs',
     },
     event = 'InsertEnter',
@@ -48,13 +49,17 @@ return {
         winhighlight = 'Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None',
       }
 
-      -- helper
-      local function has_words_before()
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
-      end
+      cmp.setup.filetype({ 'dap-repl', 'dapui_watches', 'dapui_hover' }, {
+        sources = {
+          { name = 'dap' },
+        },
+      })
 
       return {
+
+        enabled = function()
+          return vim.api.nvim_get_option_value('buftype', { buf = 0 }) ~= 'prompt' or require('cmp_dap').is_dap_buffer()
+        end,
         preselect = cmp.PreselectMode.None,
 
         formatting = {
