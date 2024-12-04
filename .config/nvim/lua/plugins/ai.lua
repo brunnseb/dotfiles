a9c8048e48c8947d4e0df5d21e5f556c87fd098d
Sebastian <sebastian.brunner@ti8m.ch>
2024-12-04 10:41:06 +0100

    Update nvim

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,26 +1,21 @@
 return {
-
   {
     'olimorris/codecompanion.nvim',
-
     dependencies = {
       { 'nvim-lua/plenary.nvim', branch = 'master' },
       'nvim-treesitter/nvim-treesitter',
-      'hrsh7th/nvim-cmp', -- Optional: For using slash commands and variables in the chat buffer
-      { 'stevearc/dressing.nvim', opts = {} }, -- Optional: Improves `vim.ui.select`
+      'hrsh7th/nvim-cmp',
+      { 'stevearc/dressing.nvim', opts = {} },
       { 'echasnovski/mini.diff', version = false },
       {
         'MeanderingProgrammer/render-markdown.nvim',
         ft = { 'markdown', 'codecompanion', 'Avante' },
-
         opts = { render_modes = true, file_types = { 'markdown', 'codecompanion', 'Avante' } },
       },
     },
     config = function()
       require('codecompanion').setup {
-        opts = {
-          -- log_level = 'DEBUG',
-        },
+        opts = {},
         display = {
           chat = {
             show_settings = true,
@@ -106,19 +101,7 @@ return {
               schema = {
                 model = {
                   default = 'bartowski_Qwen2.5-Coder-32B-Instruct-exl2',
-                  -- default = 'qwen2.5:32b-instruct-q3_K_M',
-                  -- default = 'vanilj/supernova-medius:q4_k_m',
-                  -- default = 'qwen2.5:14b-instruct-q5_0',
-                  -- default = 'mistral-nemo:12b-instruct-2407-q6_K',
-                  -- default = 'codestral:22b-v0.1-q4_0',
-                  -- default = 'deepseek-coder-v2:16b-lite-instruct-q4_1',
                 },
-                -- mirostat = {
-                --   default = 1,
-                -- },
-                -- repeat_penalty = {
-                --   default = 1,
-                -- },
                 num_ctx = {
                   default = 16384,
                 },
@@ -126,66 +109,16 @@ return {
 
               url = 'http://media:5010/v1/chat/completions',
               env = {
-                api_key = 'e79fc6f6e89fe2072e20be5a91d57b67',
-                -- url = 'http://media:5010',
+                api_key = 'TABBY_API_KEY',
               },
               headers = {
                 ['Content-Type'] = 'application/json',
               },
-              parameters = {
-                -- sync = true,
-              },
+              parameters = {},
             })
           end,
         },
       }
     end,
-  },
-  -- {
-  --   'milanglacier/minuet-ai.nvim',
-  --   config = function()
-  --     require('minuet').setup {
-  --       enabled = false,
-  --       notify = 'verbose',
-  --       provider = 'openai_compatible',
-  --       request_timeout = 20,
-  --       provider_options = {
-  --         -- openai_compatible = {
-  --         --   -- model = 'mistral-nemo:12b-instruct-2407-q6_K',
-  --         --   -- model = 'qwen2.5:14b-instruct-q5_0',
-  --         --   model = 'Handgun1773_Qwen2.5-Coder-7B-BASE-8.0bpw-exl2',
-  --         --   end_point = 'http://media:5010/v1/chat/completions',
-  --         --   api_key = 'bc2bef2f3fd5062fe2932722d50083f6',
-  --         --   name = 'Supermaven',
-  --         --   stream = true,
-  --         --   optional = {
-  --         --     -- temperature = 0.3,
-  --         --     -- mirostat = 1,
-  --         --     -- repeat_penalty = 1,
-  --         --     -- max_tokens = 8192,
-  --         --     -- stop = { '\n\n' },
-  --         --   },
-  --         -- },
-  --         openai_compatible = {
-  --           model = 'lucyknada_Qwen_Qwen2.5-Coder-1.5B-Instruct-exl2',
-  --           end_point = 'http://media:5010/v1/chat/completions',
-  --           api_key = 'bc2bef2f3fd5062fe2932722d50083f6',
-  --           name = 'Supermaven',
-  --           stream = true,
-  --         },
-  --       },
-  --     }
-  --   end,
-  -- },
-  {
-    'jondkinney/aider.nvim',
-    opts = {
-      default_bindings = false,
-    },
-    keys = {
-      -- { '<leader>an', '<cmd>AiderOpen --no-auto-commits --model=deepseek/deepseek-coder <CR>', desc = 'aider open' },
-      -- { '<leader>an', '<cmd>AiderOpen --no-auto-commits --model=codestral/codestral-latest<CR>', desc = 'aider open' },
-      { '<leader>an', '<cmd>AiderOpen --no-auto-commits --model=ollama/deepseek-coder-v2-instruct<CR>', desc = 'aider open' },
-    },
   },
 }
