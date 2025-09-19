d658bc7628bb12da9ac36c3b46192591f89661a9
Sebastian Hacisalihzade <sebastian.hacisalihzade@ti8m.ch>
2025-09-19 23:42:41 +0200

    update conf

---
+++ .config/nvim/lua/plugins/edgy.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "folke/edgy.nvim",
+    opts = {
+      right = {
+        { title = "CodeCompanion", ft = "codecompanion", size = { width = 0.4 } },
+        { title = "Grug Far", ft = "grug-far", size = { width = 0.4 } },
+      },
+    },
+  },
+}
---
+++ .config/nvim/lua/plugins/multicursor.lua
@@ -0,0 +1,41 @@
+return {
+  -- lazy.nvim:
+  {
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvimtools/hydra.nvim",
+    },
+    opts = {
+      hint_config = {
+        float_opts = {
+          border = "rounded",
+        },
+        position = "bottom-right",
+      },
+      generate_hints = {
+        normal = true,
+        insert = true,
+        extend = true,
+        config = {
+          column_count = 1,
+        },
+      },
+    },
+    cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+    keys = {
+      {
+        mode = { "v", "n" },
+        "<C-,>",
+        "<cmd>MC<cr>",
+        desc = "Create a selection for selected text or word under the cursor",
+      },
+      {
+        mode = { "v", "n" },
+        "<C-n>",
+        "<cmd>MCstart<cr>",
+        desc = "Create a selection for selected text or word under the cursor",
+      },
+    },
+  },
+}
