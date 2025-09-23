14b91739cff552ef1c9347b518bde4a365cb94a9
Sebastian Hacisalihzade <sebastian.hacisalihzade@ti8m.ch>
2025-09-23 19:30:57 +0200

    update conf

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -38,7 +38,7 @@ config = {
 	window_close_confirmation = "NeverPrompt",
 	default_prog = { "/usr/bin/fish", "-l" },
 	color_scheme = "Tokyo Night Moon",
-	font = wezterm.font_with_fallback({ { family = "MonoLisa", weight = "Medium" }, "Symbola" }),
+	font = wezterm.font_with_fallback({ { family = "MonoLisa", weight = "Regular" }, "Symbola" }),
 	line_height = 1.1,
 	inactive_pane_hsb = {
 		saturation = 0.8,
